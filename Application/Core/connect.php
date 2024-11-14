221196a946b5eded1fd522cddf4dff27795bdfae
Mario Moreno <cursotecnoweb@gmail.com>
2024-11-14 18:42:03 +0100

    Update conection to the DB

--- Application/Core/connect.php
+++ Application/Core/connect.php
@@ -11,8 +11,6 @@
 	catch(PDOException $e) {
 		$error = $e->getMessage();
 		$error_msg = "<p>Hay problemas al conectar con la base de datos, revise la configuración 
-						de acceso.</p><p>Descripción del error: <span class='error'>$error</span></p>";
-		include(SITE_ROOT . "/view/database_error.php");
-		exit();
+						de acceso.</p><p>Descripción del error: <span class='error'>$error</span></p>";		
 	}	
 ?>
