daae7040c1185885a4eb40ff5915dda76eb76e04
Mario Moreno <cursotecnoweb@gmail.com>
2025-07-30 18:05:41 +0200

    feat: remove custom loader

--- Application/Core/app_config.php
+++ Application/Core/app_config.php
@@ -3,19 +3,12 @@
 	session_regenerate_id();
 
 	/** Define root folder */
-	define("SITE_ROOT", $_SERVER['DOCUMENT_ROOT']);
-	//define('URL', $_SERVER['REQUEST_URI']);	
+	define("SITE_ROOT", $_SERVER['DOCUMENT_ROOT']);	
 
 	/** Define database configuration file */
-	define('DB_CONFIG_FILE', SITE_ROOT . '/../Application/Core/db.config.php');	
-	
-	require_once($_SERVER['DOCUMENT_ROOT'] . "/../vendor/autoload.php");
-	Application\model\classes\Loader::init($_SERVER['DOCUMENT_ROOT'] . "/..");
+	define('DB_CONFIG_FILE', SITE_ROOT . '/../Application/Core/db.config.php');				
 	
 	/** Define connection */
 	require_once(SITE_ROOT . "/../Application/Core/connect.php");
-	define('DB_CON', $dbcon);	
-
-	//session_start();
-	//session_regenerate_id();
+	define('DB_CON', $dbcon);		
 ?>
--- public/index.php
+++ public/index.php
@@ -3,9 +3,7 @@
 	
 	use Application\Core\App;
 	
-	require_once($_SERVER['DOCUMENT_ROOT'] . "/../Application/Core/aplication_fns.php");
-	
-	//Application\model\classes\Loader::init($_SERVER['DOCUMENT_ROOT'] . "/..");
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/../Application/Core/aplication_fns.php");		
 	
 	$app = new App;
 	$app->router();	
